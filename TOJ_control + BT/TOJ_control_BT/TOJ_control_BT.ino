d8795991f276c71506f74becbd5d7e7f1f379921
alexandrMperm <alex.serg.minin@gmail.com>
2017-07-19 17:59:49 +0500

    Toj BT
    удалил внешнюю библиотеку talk

--- TOJ_control + BT/TOJ_control_BT/TOJ_control_BT.ino
+++ TOJ_control + BT/TOJ_control_BT/TOJ_control_BT.ino
@@ -9,9 +9,7 @@
   ячейка EEPROM[9] - флаг вкл/выкл вентиляторы в начале пуска
   ячейка EEPROM[10] - ремя работы в первое включение
   ячейка EEPROM[11] - ремя работы основного цикла программы
-
 */
-#include "Talk.h"
 #include <Scheduler.h>
 #include <HID.h>
 #include <EEPROM.h>
@@ -24,14 +22,16 @@ String sms;                 // сообщение с терминала
 String smsbuffer = "";
 int relayPin[5] = {0, 5, 6, 7, 8}; // пины на реле 1,2,3,4
 int analogPin = 0;          // пин аналоговый вход для датчика температуры
-boolean smsResult = false;  //результат правильного сообщения с терминала
-int temp_val;               //временная переменная для вычисления
-int temp[9];                //границы вкл/выкл температуры(точки)
-int temp_toj;               //значение с датчика температуры
-byte i;                     //переменная для циклов
-int delaytime;              //время задержки основного цикла работы EEPROM 11
+boolean smsResult = false;  // результат правильного сообщения с терминала
+int temp_val;               // временная переменная для вычисления
+int temp[9];                // границы вкл/выкл температуры(точки)
+int temp_toj;               // значение с датчика температуры
+byte i;                     // переменная для циклов
+int delaytime;              // время задержки основного цикла работы EEPROM 11
 OneWire ds(4);              // пин датчика температуры
 
+const byte averageFactor = 5;   // коэффициент сглаживания показаний (0 не ставить!)
+                                // чем выше, тем больше "инерционность"
 //шота для обмена по шине CAN
 long unsigned int rxId;
 unsigned char len = 0;
@@ -51,7 +51,7 @@ void setup() {
   Serial.begin(115200);      // открывает последовательный порт, устанавливает скорость 9600 бит
   //Serial.setTimeout(20);
   CAN0.begin(CAN_500KBPS);   // init can bus : baudrate = 500k
-  pinMode(13, OUTPUT);		 	
+  pinMode(13, OUTPUT);
   pinMode(9, INPUT);         //пин 9 INT MCP модуля
   // pinMode(4,INPUT);       // пин 4 датчика температуры
   for (i = 1; i <= 4; i++)   // инициализация пинов реле
@@ -59,12 +59,12 @@ void setup() {
 
   // реле на LOW,  будут включать вентилятор
   // реле на HIGH,  будут выключать вентилятор
-    
+
   delaytime = EEPROM.read(11) * 1000;
 
   //вкл/выкл вентиляторы в начале пуска
   if (EEPROM.read(9) == 1) {
-    RelayState(1, 1, 1, 1);        //включаем все вентиляторы	
+    RelayState(1, 1, 1, 1);        //включаем все вентиляторы
 	Blick(EEPROM.read(10) * 1000); //время работы в начале пуска(cooltime)
 
     /* отключаем вентиляторы последовательно для проверки
@@ -77,12 +77,17 @@ void setup() {
 
   for (i = 1; i <= 8; i++) //считываем точки температуры из постоянной памяти
     temp[i] = EEPROM.read(i);
+
+  Scheduler.startLoop(loopTalk);
+  Scheduler.startLoop(loopTempToj);
+  //Scheduler.startLoop(loopTemp);
+  //Scheduler.startLoop(loopMCPRecive);
 }
 
 // перезагрузка
 void(* resetFunc) (void) = 0;
 
-/* Общение с внешним миром 
+/* Общение с внешним миром
   без возвращения перем.
   Команда - РЕЗУЛЬТАТ
   tmpNtoj   шаблон сообщения для задания точки температуры
@@ -98,18 +103,19 @@ void(* resetFunc) (void) = 0;
   cooltimeX установить время "X" работы вентиляторов при начале работы (проверка работы) от 1 до 9сек!
   0         узначть значение cooltime в eeprom
   check     флаг вкл/выкл вентиляторы в начале пуска
+  manualXX  включить вручную в определенном состоянии 01..4 отдельные реле. 05-14 для приложения
 */
 //xxxMmMmMmCdC
 //234567890123
 //2-------3---
-void Talk() {
+void loopTalk() {
   if (Serial.available() > 0 || smsbuffer.length() > 0) {   //если есть сообщение
-    
-	//добавляем сообщения в буфер
+
+	  //добавляем сообщения в буфер
     smsbuffer = smsbuffer + char('\n') + Serial.readString();
-        
-    //выбираем из буфера строку 
-	sms = "";
+
+    //выбираем из буфера строку
+	  sms = "";
     while ( byte(smsbuffer[0]) != 10 && smsbuffer.length() > 0){
       sms = sms + smsbuffer[0];
       smsbuffer.remove(0,1);
@@ -117,7 +123,7 @@ void Talk() {
 
     if ( smsbuffer[0] == char('\n') ) //убираем перенос после строки
       smsbuffer.remove(0,1);
-      
+
     Serial.print("I get->");
     Serial.println(sms);
     smsResult = false;
@@ -125,18 +131,71 @@ void Talk() {
     if (sms.substring(0, 2) == "on") {    // включаем все реле
       manual = -1;
       Serial.println("manual on");
-	  RelayState(1, 1, 1, 1);
+	    RelayState(1, 1, 1, 1);
       smsResult = true;
     }
     if (sms.substring(0, 3) == "off") { // выключаем все реле
       manual = -2;
       Serial.println("manual off");
-	  RelayState(0, 0, 0, 0);
+	    RelayState(0, 0, 0, 0);
       smsResult = true;
     }
-	talk_no();
-	talk_manual();
-
+    if (sms.substring(0, 2) == "no") {
+  		manual = 0;
+  		Serial.println("manual no");
+  		smsResult = true;
+  	}
+    // 14 положений отдельного включения
+    if (sms.substring(0, 6) == "manual") {
+  		manual = (int(sms[7]) - 48) * 10 + int(sms[6]) - 48;
+        switch (manual) {
+          case 1:
+            RelayState(1,0,0,0);
+            break;
+          case 2:
+            RelayState(0,1,0,0);
+            break;
+          case 3:
+            RelayState(0,0,1,0);
+            break;
+          case 4:
+            RelayState(0,0,0,1);
+            break;
+          case 5:
+            RelayState(1,1,0,0);
+            break;
+          case 6:
+            RelayState(1,0,1,0);
+            break;
+          case 7:
+            RelayState(1,0,0,1);
+            break;
+          case 8:
+            RelayState(0,1,1,0);
+            break;
+          case 9:
+            RelayState(0,1,0,1);
+            break;
+          case 10:
+            RelayState(0,0,1,1);
+            break;
+          case 11:
+            RelayState(1,1,1,0);
+            break;
+          case 12:
+            RelayState(1,1,0,1);
+            break;
+          case 13:
+            RelayState(1,0,1,1);
+            break;
+          case 14:
+            RelayState(0,1,1,1);
+            break;
+          default:
+            manual = 0;
+          break;
+        }
+  	}
     //задание точки температуры
     //N - порядковый номер точки, ххх - трехзначное число с датчика температуры
     //tmpNxxx - set tempreture of dot, N - nomber of dot, xxx - temp of dot
@@ -145,7 +204,6 @@ void Talk() {
       EEPROM.write(int(sms[3]) - 48, byte((int(sms[4]) - 48) * 100 + (int(sms[5]) - 48) * 10 + (int(sms[6]) - 48)));
       smsResult = true;
     }
-
     if (sms.substring(0, 5) == "flash") {  //вывод в терминал из памяти сохраненных точек
       Serial.println("Thats what in EEPROM now:");
       Blick(150);
@@ -166,7 +224,6 @@ void Talk() {
       Serial.println(EEPROM.read(11));
       smsResult = true;
     }
-
     if (sms.substring(0, 3) == "ram") {  //вывод в терминал из оперативки сохраненных точек
       Serial.println("Thats what in RAM now:");
       Blick(150);
@@ -179,7 +236,6 @@ void Talk() {
       }
       smsResult = true;
     }
-
     if (sms.substring(0, 5) == "check") {
       if (EEPROM.read(9) != 0) {
         EEPROM.write(9, 0);
@@ -193,7 +249,6 @@ void Talk() {
       }
       smsResult = true;
     }
-
     if (sms.substring(0, 8) == "cooltime") {
       if (sms[8] == '0')  {
         Serial.print("cooltime now -> ");
@@ -208,7 +263,6 @@ void Talk() {
         Serial.println("cooltime time must be [0..9] ");
       smsResult = true;
     }
-
     if (sms.substring(0, 9) == "delaytime") {
       if (sms[9] == '0') {
         Serial.print("delaytime now -> ");
@@ -224,10 +278,9 @@ void Talk() {
         Serial.println("delay time must be [0..9] ");
       smsResult = true;
     }
-
     if (sms.substring(0, 4) == "help") {
       Serial.println("on/off/no - to manual switch all relay");
-      Serial.println("manualN - to manual turn On only N number relay");
+      Serial.println("manualN - to manual turn On only N number relay. 5-14 some variants to on");
       Serial.println("tmpNxxx - set tempreture of dot, N - number of dot, xxx - temp of dot");
       Serial.println("flash - read settings from EEPROM");
       Serial.println("ram - read dots from RAM");
@@ -246,9 +299,10 @@ void Talk() {
       Serial.println("print help for help");
     }
   }
+  yield();
 }
 
-//вывод температуры в распред коробке
+// вывод температуры в распред коробке
 void Temp() {
   byte data[2];
   ds.reset();
@@ -264,11 +318,11 @@ void Temp() {
   Temp = Temp >> 4;
   Serial.print("Temperature ");
   Serial.println(Temp);
+  yield();
 }
 
-//MCP CAN  BUS
-void MCPRecive()
-{
+// MCP CAN  BUS
+void loopMCPRecive(){
   if (!digitalRead(2))                        // If pin 2 is low, read receive buffer
   {
     CAN0.readMsgBuf(&len, rxBuf);              // Read data: len = data length, buf = data byte(s)
@@ -287,9 +341,10 @@ void MCPRecive()
     }
     Serial.println();
   }
+  yield();
 }
 
-//включение реле если 1 то включает
+// включение реле если 1 то включает
 void RelayState(boolean r1, boolean r2, boolean r3, boolean r4) {
   if (r1) digitalWrite(relayPin[1], LOW);
   else digitalWrite(relayPin[1], HIGH);
@@ -304,9 +359,16 @@ void RelayState(boolean r1, boolean r2, boolean r3, boolean r4) {
   else digitalWrite(relayPin[4], HIGH);
 }
 
+void loopTempToj() {
+  int oldtemp_toj = temp_toj;
+  temp_toj = analogRead(analogPin);
+  // усреднение значение с датчика температуры ОЖ
+  temp_toj = (oldtemp_toj * (averageFactor - 1) + temp_toj) / averageFactor;
+  yield();
+}
+
 void loop() {
 
-  temp_toj = analogRead(analogPin); //читаем тож
   sms = "toj_temp_";
   sms += temp_toj;
   Serial.println(sms);
@@ -317,22 +379,19 @@ void loop() {
 
   // включения 1 обмотки 1го мотора = лёгкий обдув
   //if (temp >= 82 && temp < 85 && hardon != 1) {
-  if (temp_toj >= temp[2] && temp_toj < temp[3] && manual == 0) RelayState(1, 0, 0, 0);
+  if (temp_toj >= temp[3] && temp_toj < temp[2] && manual == 0) RelayState(1, 0, 0, 0);
 
   // включает 1 мотор полность
   //if (temp >= 85 && temp < 87 && hardon != 1) {
-  if (temp_toj >= temp[4] && temp_toj < temp[5] &&  manual == 0) RelayState(1, 1, 0, 0);
+  if (temp_toj >= temp[4] && temp_toj < temp[6] &&  manual == 0) RelayState(1, 1, 0, 0);
 
   // включает 1 мотор полность и половину второго
   //if (temp >= 87 && temp < 90 && hardon != 1) {
-  if (temp_toj >= temp[6] && temp_toj < temp[7] &&  manual == 0) RelayState(1, 1, 1, 0);
+  if (temp_toj >= temp[7] && temp_toj < temp[6] &&  manual == 0) RelayState(1, 1, 1, 0);
 
   // включает два мотора на полную
   //if (temp >= 90) {
   if (temp_toj < temp[8] &&  manual == 0) RelayState(1, 1, 1, 1);
 
-  Talk();                   //читаем сообщение с ПК
-  //Temp();                   //меряем температуру
-  //MCPRecive();              //читаем сообщение по can шине
-  Blick(delaytime);         //таймаут
+  Blick(delaytime); // таймаут
 }
